128884bd2c6d6b7aecb2ed4e1c0711657c8ae9d9
Kostarkis <62484365+Kostarkis@users.noreply.github.com>
2020-03-22 11:15:33 +0100

    Delete database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(!isset($_SESSION['databaseFileLocker']))
-    header('Location: index.php?info=nice_try_hacker');
-$host = "127.0.0.1";
-$db_user = "pbieniek_gsidkdatabase";
-$db_password = "Szuberwlustrze123";
-$db_name = "pbieniek_gsidkdatabase";
-?>
